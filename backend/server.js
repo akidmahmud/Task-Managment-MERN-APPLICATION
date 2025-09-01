d69b9b0e80c2b3089522e80ae2d30bbe3e00a52a
Mahmud <131474126+akidmahmud@users.noreply.github.com>
2025-09-01 11:29:03 +0600

    backend Init

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require ("express");
+const cors = require ("cors");
+const path = require("path");
+
+const app = express();
+
+//MiddleWare to handle CORS
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods : ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"]
+    })
+);
+
+//Middleware
+app.use(express.json());
+
+//Routes
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=> console.log(`server is running on ${PORT}`));
